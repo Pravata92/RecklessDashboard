711f8260c71d75817acf64dacda946451ef6d602
samielee86 <samielee86@gmail.com>
2021-03-05 21:02:40 -0300

    en proceso de renderizar cantidad de productos por categoria

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -3,6 +3,7 @@ import PageHeading from '../PageHeading'
 import LastAdded from '../sections/LastAdded'
 import Label from '../sections/Label'
 import { useState, useEffect } from 'react'
+// import img_product from '../../../public/img/product_dummy.svg'
 
 export default function Main(){
 
@@ -10,7 +11,9 @@ export default function Main(){
     const [ totalPrice, setTotalPrice] = useState('0.00')
     const [ totalUsers, setTotalUsers] = useState('0')
     const [ totalCategories, setTotalCategories] = useState('0')
-    const [ lastProduct, setLastProduct] = useState()
+    const [ name, setName] = useState('Title')
+    const [ description, setDescription] = useState('Description')
+    const [ images, setImages] = useState('Image')
 
       useEffect(()=>{
           fetch('http://localhost:3300/api/products')
@@ -20,7 +23,11 @@ export default function Main(){
                     setQty(data.allProducts.count)
                     setTotalPrice(data.meta.totalAmount)
                     setTotalCategories(data.meta.totalCategories)
-                    setLastProduct(data.meta.lastProduct)
+                    const { name, description, images_url } = data.meta.lastProduct
+                    setName(name)
+                    setDescription(description)
+                    setImages(images_url[0])
+                    
                   })
           fetch('http://localhost:3300/api/users')
               .then(res => res.json())
@@ -38,7 +45,8 @@ export default function Main(){
                     <Label color="primary" title="products in data base" content={qty} item="fas fa-clipboard-list fa-2x text-gray-500"/>
                     <Label color="warning" title="total categories" content={totalCategories} item="fas fa-tshirt fa-2x text-gray-500"/>
                   <Container classes="row">
-                    <LastAdded element="product" color="primary" imgURL="LA IMAGEN" name={lastProduct.name} description={lastProduct.description}/>
+                    <LastAdded element="product" color="primary" imgURL={images} name={name} description={description}/>
+                    <Table element="product" color="primary" imgURL={images} name={name} description={description}/>
                   </Container>
                 </Container>
           	</Container>
